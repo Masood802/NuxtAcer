69830b7278c7eaccaadc7838ad8a4b37b711a809
Masood <masoodsse@gmail.com>
2024-04-25 10:35:18 +0500

    progress bar animation

--- components/slider.vue
+++ components/slider.vue
@@ -8,16 +8,43 @@ let i = ref(0);
 onMounted(() => {
   setInterval(() => {
     i.value = i.value + 1;
-    if (i.value == 3) {
+    if(i.value===1){
+      document.getElementById('div1').style.width='48px'
+      document.getElementById('div1').style.transition='width 4.5s linear'
+      document.getElementById('div3').style.width='0px'
+      document.getElementById('div3').style.transition='none'
+    }
+    else if(i.value===2){
+      document.getElementById('div2').style.width='48px'
+      document.getElementById('div2').style.transition='width 4.5s linear'
+      document.getElementById('div1').style.width='0px'
+      document.getElementById('div1').style.transition='none'
+    }
+    else if(i.value===3){
+      document.getElementById('div3').style.width='48px'
+      document.getElementById('div3').style.transition='width 4.5s linear'
+      document.getElementById('div2').style.width='0px'
+      document.getElementById('div2').style.transition='none'
+    }
+    if (i.value === 3) {
       i.value = 0;
+      console.log('0')
     }
   }, 5000);
 });
 </script>
 <template>
-  <div
-    class=""
-  >
+  <div class="my-10 relative">
       <img :src="images[i]" alt="" class="" />
+      <div class="absolute bg-white w-12 h-[2px] left-[42%] bottom-3 z-40">
+        <span id="div1" class="absolute bottom-0 w-0 bg-red-700 h-[2px] z-50 left[42%] "></span>
+      </div>
+      <div class="absolute bg-white w-12 h-[2px] left-[46%] bottom-3 z-40 mr-1">
+        <span id="div2" class="absolute bottom-0 w-0 bg-red-700 h-[2px] z-50 left[46%] "></span>
+      </div>
+      <div class="absolute bg-white w-12 h-[2px] left-[50%] bottom-3 z-40">
+        <span id="div3" class="absolute bottom-0 w-0 bg-red-700 h-[2px] z-50 left[506%]"></span>
+      </div>
+
   </div>
 </template>
