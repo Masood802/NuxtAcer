085a68e9b6ecbc3f93319c9f356b2de285742200
Masood <masoodsse@gmail.com>
2024-04-25 12:37:16 +0500

    about page coding complete

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-
+    <div class="mt-20  bg-[url('http://localhost:3000/_nuxt/assets/images/about.jpg')] relaive bg-center-top w-full h-screen bg-cover ">
+        <div class="w-[50%] h-[450px] bg-white shadow-md mx-10 absolute top-[35%] px-10 ">
+            <h2 class="text-3xl font-semibold py-10 text-zinc-700">Acer Storage</h2>
+            <p class="text-lg text-zinc-500 ">Acer branded storage products are designed and manufactured by
+                 BIWIN Storage Technology company, 
+                a leading maker of quality flash memory, DRAM memory and 
+                solid-state drives (SSD storage devices).</p>
+             <p class="text-lg text-zinc-500 mt-5">
+                Founded in 2010, BIWIN focuses on providing global customers with 
+                high-quality storage products and specializes in the research,
+                 development, manufacturing and sales of memory chip application products. 
+             </p>   
+        </div>
     </div>
 </template>
 
