aef409671ea127853ce0d0c200e47593e9e26a8c
Malik Masood <masoodsse@gmail.com>
2024-04-27 10:24:12 +0500

    new property type added in products

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -8,14 +8,14 @@
   </div>
 <div class="flex items-center w-full h-40 bg-zinc-200">
   <label for="products" class="uppercase ml-4">Product Type:</label>
-  <select name="products" id="prd" class="mx-2 bg-white shadow-lg w-44 h-8 my-2 p-1 text-sm outline-none">
+  <select name="products" id="prd" v-model="store.selectedType" onselect="showItems()" >
     <option value="ALL" class="py-2">ALL</option>
     <option value="PCIe M.2 SSD">PCIe M.2 SSD</option>
     <option value="SATA 2.5 SSD">SATA 2.5 SSD</option>
     <option value="SATA M.2 SSD">SATA 2 SSD</option>
-    <option value="">Memory</option>
-    <option value="">Memory Card</option>
-    <option value="">USB Flash Drive</option>
+    <option value="Memory">Memory</option>
+    <option value="Memory Card">Memory Card</option>
+    <option value="USB Flash Drive">USB Flash Drive</option>
   </select>
 </div>
 <div class="grid grid-cols-3 grid-rows-4 mx-auto my-10">
@@ -31,7 +31,41 @@
 <script setup>
 import { useProductStore } from '~/stores/productStore';
 
-const store=useProductStore()
+const store = useProductStore()
+function showItems() {
+  if (store.selectedType === 'Memory') {
+    store.products.filter((item)=>item.type!=store.selectedType)
+  }
+}
 </script> 
 
-<style scoped></style>
+<style scoped>
+select {
+  -webkit-appearance: button;
+  -moz-appearance: button;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -webkit-padding-end: 20px;
+  -moz-padding-end: 20px;
+  -webkit-padding-start: 2px;
+  -moz-padding-start: 2px;
+  background-color: #d7cece; /* Fallback color if gradients are not supported */
+  background-image: url(../images/select-arrow.png), -webkit-linear-gradient(top, #E5E5E5, #F4F4F4); /* For Chrome and Safari */
+  background-image: url(../images/select-arrow.png), -moz-linear-gradient(top, #E5E5E5, #F4F4F4); /* For old Firefox (3.6 to 15) */
+  background-image: url(../images/select-arrow.png), -ms-linear-gradient(top, #E5E5E5, #F4F4F4); /* For pre-releases of Internet Explorer  10*/
+  background-image: url(../images/select-arrow.png), -o-linear-gradient(top, #E5E5E5, #F4F4F4); /* For old Opera (11.1 to 12.0) */
+  background-image: url(../images/select-arrow.png), linear-gradient(to bottom, #E5E5E5, #F4F4F4); /* Standard syntax; must be last */
+  background-position: center right;
+  background-repeat: no-repeat;
+  border: 1px solid #e3dada;
+  border-radius: 2px;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1);
+  color: #141313dc;
+  font-size: inherit;
+  margin: 2px 4px;
+  overflow: hidden;
+  padding: 2px 20px ;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
--- stores/productStore.js
+++ stores/productStore.js
@@ -4,95 +4,111 @@ export const useProductStore = defineStore("product", {
         products: [
             {
                 id: 1,
-                title: 'Acer UD200 DDR5 Desktop Memory',
+                type:'PCIe M.2 SSD',
+                title: 'Acer MA200 NVMe SSD ',
                 image: 'http://localhost:3000/_nuxt/assets/images/Acer_MA200.jpg',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 2,
-                title: 'Acer SD200 Laptop DRAM',
+                type:'Memory',
+                title: 'Acer UD200 DDR5 Desktop Memory',
                 image:'http://localhost:3000/_nuxt/assets/images/Acer_UD200.jpg',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
-                id: 3,
-                title: 'Acer FA200 PCle M.2 SSD',
+              id: 3,
+                type:'Memory',
+                title: 'Acer SD200 Laptop DRAM',
                 image:'http://localhost:3000/_nuxt/assets/images/Acer_SD200.jpg',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 4,
-                title: 'Acer UM310 USB Flash Drive',
+                type:'PCIe M.2 SSD',
+                title: 'Acer FA200 PCle M.2 SSD',
                 image:'http://localhost:3000/_nuxt/assets/images/product-3.png',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 5,
+                type:'Memory Card',
                 title: 'Acer MSC300 MicroSD Card',
                 image:'http://localhost:3000/_nuxt/assets/images/1n.jpg',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 6,
-                title: 'Acer UP300 USB Flash Drive',
+                type:'USB Flash Drive',  
+                title: 'Acer UM310 USB Flash Drive',
                 image:'http://localhost:3000/_nuxt/assets/images/UM310.png',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 7,
+                type:'USB Flash Drive',
                 title: 'Acer UP300 USB Flash Drive',
                 image:'http://localhost:3000/_nuxt/assets/images/product-4.jpg',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 8,
+                type:'USB Flash Drive',
                 title: 'Acer UP200 USB Flash Drive',
                 image:'http://localhost:3000/_nuxt/assets/images/01.jpg',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 9,
+                type:'USB Flash Drive',
                 title: 'Acer UF300 USB Flash Drive',
                 image:'http://localhost:3000/_nuxt/assets/images/6.png',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 10,
+                type:'USB Flash Drive',
                 title: 'Acer UF200 USB Flash Drive',
                 image:'http://localhost:3000/_nuxt/assets/images/6.png',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 11,
+                type:'Memory Card',
                 title: 'Acer SC300 SD Card',
                 image:'http://localhost:3000/_nuxt/assets/images/1.png',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 12,
+                type:'Memory Card',
                 title: 'Acer SC900 SD Card',
                 image:'http://localhost:3000/_nuxt/assets/images/010.png',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
              },
             {
                 id: 13,
+                type:'Memory',
                 title: 'Acer HT100 Desktop DRAM',
                 image:'http://localhost:3000/_nuxt/assets/images/7.jpg',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 14,
-                title: 'Acer CFexpress card CFE100',
+                type:'Memory Card',
+                title: 'Acer CF express card CFE100',
                 image:'http://localhost:3000/_nuxt/assets/images/CFE.jpg',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
           },
             {
                 id: 15,
+                type:'Memory Card',
                 title: 'Acer CFE100 Memory Card',
                 image:'http://localhost:3000/_nuxt/assets/images/1T.jpg',
                 description:'Unleash speeds up to 5600 MHz Elevate stability with On-Die ECC Power management IC (PMIC)'
             },
-      ]
+    ],
+    selectedType:'All'
   }),
    actions: {
     },
